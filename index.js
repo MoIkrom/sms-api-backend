0534d7b29d484477e5320609784de1b265134b86
moikrom <mikram2162gmail.com>
2024-06-03 16:39:41 +0700

    fix env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware untuk menguraikan application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
