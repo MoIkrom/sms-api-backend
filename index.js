2ac8fb620ea4ebb3fd65b24e27d24ab4880d0bd6
moikrom <mikram2162gmail.com>
2024-06-04 12:24:13 +0700

    add cors

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+
 // Middleware untuk menguraikan body dari request POST yang menggunakan application/x-www-form-urlencoded dan JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Middleware CORS
+app.use(cors());
+
 app.post("/webhook", (req, res) => {
-  console.log("Received webhook:", req.body);
+  // console.log("Received webhook:", req.body);
+  console.log(res.params);
   res.sendStatus(200);
 });
 
